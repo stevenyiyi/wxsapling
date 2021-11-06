e4bb6a8de8eb24bc4a106a79dae4d686c5629073
stevenyiyi <104037806@qq.com>
2021-11-06 22:37:54 +0800

    add liveplayer.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./styles.css";
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./components/home_page";
---
+++ src/components/live_player.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./common.css";
+export default function LivePlayer(props) {
+  return <div className="container"></div>;
+}
