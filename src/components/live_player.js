1f9dec2f6ea830243664b67f07df2745b06d652c
stevenyiyi <104037806@qq.com>
2021-11-06 22:43:22 +0800

    update live_player

--- src/components/live_player.js
+++ src/components/live_player.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "./common.css";
 export default function LivePlayer(props) {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="player"></div>
+      <div className="camera_list"></div>
+      <div className="bottom_bar"></div>
+    </div>
+  );
 }
