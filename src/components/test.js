4335901d8df0186456784b1852afb6fa94d04b6a
stevenyiyi <104037806@qq.com>
2021-12-11 23:39:07 +0800

    Add barrage

--- src/components/test.js
+++ src/components/test.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./test.css";
+import "./barrage.css";
 export default function Test(props) {
   return (
     <div className="container">
-      <div className="player">
-        <div className="video__container">
-          <video
-            id="myvideo"
-            controls={true}
-            autoPlay={true}
-            poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/poster-img.jpg"
-            preload="metadata"
-          >
-            <source src="https://d.pr/f/On5R8M+" type="video/webM" />
-            <p>Your browser does not support the HTML5 Video Element</p>
-          </video>
+      <div className="video__container">
+        <video
+          id="myvideo"
+          controls={true}
+          autoPlay={true}
+          poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/poster-img.jpg"
+          preload="metadata"
+        >
+          <source src="https://d.pr/f/On5R8M+" type="video/webM" />
+          <p>Your browser does not support the HTML5 Video Element</p>
+        </video>
+        <div className="track track-1">柱子，你他娘了怎么不省点炮弹!</div>
+        <div className="track track-2">
+          来，把老子的意大利炮抬上来，给柱子点个烟
         </div>
+        <div className="track track-3">小的们，吃唐僧肉啦</div>
       </div>
-      <div className="content__container">fdasadsfasdfafsadfasfa</div>
     </div>
   );
 }
