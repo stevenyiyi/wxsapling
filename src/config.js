f52ffc6accc2aa4408801ff6c5f8761dfcb6bd20
stevenyiyi <104037806@qq.com>
2022-02-10 10:56:23 +0800

    adsfasdfa

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  apiBaseUrl:
+    process.env.NODE_ENV === "production"
+      ? "https://wx.anylooker.com/"
+      : "https://localhost/",
+  resBaseUrl:
+    process.env.NODE_ENV === "production"
+      ? "https://anylooker.com"
+      : "https://localhost/",
+  googleWebClientId:
+    "947434104376-39t4p666elc7s9tlc5ac7hnec6g3oips.apps.googleusercontent.com"
+};
