3fd2a96e41150b4d2bbf2b3ba477f0067d965bcb
stevenyiyi <104037806@qq.com>
2022-03-10 18:34:13 +0800

    asfasdfas

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 export default {
   apiBaseUrl:
     process.env.NODE_ENV === "production"
-      ? "https://localhost/"///"https://wx.anylooker.com/"
+      ? "https://wx.anylooker.com/"
       : "https://localhost/",
   resBaseUrl:
     process.env.NODE_ENV === "production"
-      ? "https://localhost/"///"https://anylooker.com/"
+      ? "https://anylooker.com/"
       : "https://localhost/",
   wssGroupChatUrl:
     process.env.NODE_ENV === "production"
-      ? "wss://localhost/ws_group_chat" ///wss://anylooker.com/ws_group_chat"
+      ? "wss://anylooker.com/ws_group_chat"
       : "wss://localhost/ws_group_chat"
 };
